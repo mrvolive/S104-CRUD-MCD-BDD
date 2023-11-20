d0b9de4303b39a665dc93c9a6399e508c18745f1
mrvolive <olivier.maraval@edu.univ-fcomte.fr>
2023-11-20 08:49:48 +0100

    Structure du app.py

--- site/app.py
+++ site/app.py
@@ -1,14 +1,15 @@
 #! /usr/bin/python
 # -*- coding:utf-8 -*-
-from flask import Flask, request, render_template, redirect, flash
 
-app = Flask(__name__)
-app.secret_key = 'une cle(token) : grain de sel(any random string)'
-
-                                    ## à ajouter
-from flask import session, g
+# == Importations == #
+from flask import Flask, request, render_template, redirect, session, g, flash
 import pymysql.cursors
 
+# == Configuration == #
+app = Flask(__name__)
+app.secret_key = 'azerty'
+
+# == Connexion à la base de données == #
 def get_db():
     if 'db' not in g:
         g.db =  pymysql.connect(
@@ -21,13 +22,16 @@ def get_db():
         )
     return g.db
 
+# == Déconnexion de la base de données == #
 @app.teardown_appcontext
 def teardown_db(exception):
     db = g.pop('db', None)
     if db is not None:
         db.close()
 
-app = Flask(__name__)    # instance de classe Flask (en paramètre le nom du module)
+################
+# == Routes == #
+################
 
 @app.route('/')
 @app.route('/etudiant/show')
@@ -107,5 +111,9 @@ def valid_edit_etudiant():
     get_db().commit()
     return redirect('/etudiant/show')
 
+
+####################################
+# == Lancement de l'application == #
+####################################
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
