6cf53419bfaad33764d27a9cb82f9ce239d27279
Olivier MARAVAL <144356778+mrvolive@users.noreply.github.com>
2023-11-21 11:09:30 +0100

    Mise en place de la structure du site

--- site/app.py
+++ site/app.py
@@ -34,82 +34,97 @@ def teardown_db(exception):
 ################
 
 @app.route('/')
-@app.route('/etudiant/show')
-def show_etudiants():
-    mycursor = get_db().cursor()
-    sql=''' SELECT id_etudiant AS id, nom_etudiant AS nom, groupe_etudiant AS groupe
-    FROM etudiant
-    ORDER BY nom_etudiant;'''
-    mycursor.execute(sql)
-    liste_etudiants = mycursor.fetchall()
-    return render_template('etudiant/show_etudiants.html', etudiants=liste_etudiants )
+
+# == Routes appartements == #
+
+# == Routes consommations == #
+
+# == Routes contrats == #
+
+# == Routes locataires == #
 
 
-@app.route('/etudiant/add', methods=['GET'])
-def add_etudiant():
-    print('''affichage du formulaire pour saisir un étudiant''')
-    return render_template('etudiant/add_etudiant.html')
+#########################
+# == Routes Exemples == #
+#########################
 
-@app.route('/etudiant/delete')
-def delete_etudiant():
-    print('''suppression d'un étudiant''')
-    id=request.args.get('id',0)
-    print(id)
-    mycursor = get_db().cursor()
-    tuple_param=(id)
-    sql="DELETE FROM etudiant WHERE id_etudiant=%s;"
-    mycursor.execute(sql,tuple_param)
-
-    get_db().commit()
-    print(request.args)
-    print(request.args.get('id'))
-    id=request.args.get('id',0)
-    return redirect('/etudiant/show')
-
-@app.route('/etudiant/edit', methods=['GET'])
-def edit_etudiant():
-    print('''affichage du formulaire pour modifier un étudiant''')
-    print(request.args)
-    print(request.args.get('id'))
-    id=request.args.get('id')
-    mycursor = get_db().cursor()
-    sql=''' SELECT id_etudiant AS id, nom_etudiant AS nom, groupe_etudiant AS groupe
-    FROM etudiant
-    WHERE id_etudiant=%s;'''
-    tuple_param=(id)
-    mycursor.execute(sql,tuple_param)
-    etudiant = mycursor.fetchone()
-    return render_template('etudiant/edit_etudiant.html', etudiant=etudiant)
+# @app.route('/')
+# @app.route('/etudiant/show')
+# def show_etudiants():
+#     mycursor = get_db().cursor()
+#     sql=''' SELECT id_etudiant AS id, nom_etudiant AS nom, groupe_etudiant AS groupe
+#     FROM etudiant
+#     ORDER BY nom_etudiant;'''
+#     mycursor.execute(sql)
+#     liste_etudiants = mycursor.fetchall()
+#     return render_template('etudiant/show_etudiants.html', etudiants=liste_etudiants )
 
 
-@app.route('/etudiant/add', methods=['POST'])
-def valid_add_etudiant():
-    print('''ajout de l'étudiant dans le tableau''')
-    nom = request.form.get('nom')
-    groupe = request.form.get('groupe')
-    message = 'nom :' + nom + ' - groupe :' + groupe
-    print(message)
-    mycursor = get_db().cursor()
-    tuple_param=(nom,groupe)
-    sql="INSERT INTO etudiant(id_etudiant, nom_etudiant, groupe_etudiant) VALUES (NULL, %s, %s);"
-    mycursor.execute(sql,tuple_param)
-    get_db().commit()
-    return redirect('/etudiant/show')
+# @app.route('/etudiant/add', methods=['GET'])
+# def add_etudiant():
+#     print('''affichage du formulaire pour saisir un étudiant''')
+#     return render_template('etudiant/add_etudiant.html')
 
-@app.route('/etudiant/edit', methods=['POST'])
-def valid_edit_etudiant():
-    print('''modification de l'étudiant dans le tableau''')
-    id = request.form.get('id')
-    nom = request.form.get('nom')
-    groupe = request.form.get('groupe')
-    message = 'nom :' + nom + ' - groupe :' + groupe + ' pour l etudiant d identifiant :' + id
-    print(message)
-    mycursor = get_db().cursor()
-    tuple_param=(nom,groupe,id)
-    sql="UPDATE etudiant SET nom_etudiant = %s, groupe_etudiant= %s WHERE id_etudiant=%s;"
-    mycursor.execute(sql,tuple_param)
-    get_db().commit()
-    return redirect('/etudiant/show')
+# @app.route('/etudiant/delete')
+# def delete_etudiant():
+#     print('''suppression d'un étudiant''')
+#     id=request.args.get('id',0)
+#     print(id)
+#     mycursor = get_db().cursor()
+#     tuple_param=(id)
+#     sql="DELETE FROM etudiant WHERE id_etudiant=%s;"
+#     mycursor.execute(sql,tuple_param)
+
+#     get_db().commit()
+#     print(request.args)
+#     print(request.args.get('id'))
+#     id=request.args.get('id',0)
+#     return redirect('/etudiant/show')
+
+# @app.route('/etudiant/edit', methods=['GET'])
+# def edit_etudiant():
+#     print('''affichage du formulaire pour modifier un étudiant''')
+#     print(request.args)
+#     print(request.args.get('id'))
+#     id=request.args.get('id')
+#     mycursor = get_db().cursor()
+#     sql=''' SELECT id_etudiant AS id, nom_etudiant AS nom, groupe_etudiant AS groupe
+#     FROM etudiant
+#     WHERE id_etudiant=%s;'''
+#     tuple_param=(id)
+#     mycursor.execute(sql,tuple_param)
+#     etudiant = mycursor.fetchone()
+#     return render_template('etudiant/edit_etudiant.html', etudiant=etudiant)
+
+
+# @app.route('/etudiant/add', methods=['POST'])
+# def valid_add_etudiant():
+#     print('''ajout de l'étudiant dans le tableau''')
+#     nom = request.form.get('nom')
+#     groupe = request.form.get('groupe')
+#     message = 'nom :' + nom + ' - groupe :' + groupe
+#     print(message)
+#     mycursor = get_db().cursor()
+#     tuple_param=(nom,groupe)
+#     sql="INSERT INTO etudiant(id_etudiant, nom_etudiant, groupe_etudiant) VALUES (NULL, %s, %s);"
+#     mycursor.execute(sql,tuple_param)
+#     get_db().commit()
+#     return redirect('/etudiant/show')
+
+# @app.route('/etudiant/edit', methods=['POST'])
+# def valid_edit_etudiant():
+#     print('''modification de l'étudiant dans le tableau''')
+#     id = request.form.get('id')
+#     nom = request.form.get('nom')
+#     groupe = request.form.get('groupe')
+#     message = 'nom :' + nom + ' - groupe :' + groupe + ' pour l etudiant d identifiant :' + id
+#     print(message)
+#     mycursor = get_db().cursor()
+#     tuple_param=(nom,groupe,id)
+#     sql="UPDATE etudiant SET nom_etudiant = %s, groupe_etudiant= %s WHERE id_etudiant=%s;"
+#     mycursor.execute(sql,tuple_param)
+#     get_db().commit()
+#     return redirect('/etudiant/show')
 
 
 ####################################
