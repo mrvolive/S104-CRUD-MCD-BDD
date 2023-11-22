ae3b1ec6003b5f9a70e4d074a54045c1356a707a
Olivier MARAVAL <144356778+mrvolive@users.noreply.github.com>
2023-11-22 18:16:37 +0100

    Finalisation CRUD consommation

--- site/app.py
+++ site/app.py
@@ -49,10 +49,10 @@ def show_index():
 @app.route("/consommations/show")
 def show_consommations():
     mycursor = get_db().cursor()
-    sql = """SELECT id_consomme AS id, date_conso AS date, quantite_consomme AS quantite, libelle_consommable AS type, num_appartement AS appartement
+    sql ='''SELECT id_consomme AS id, date_conso AS date, quantite_consomme AS quantite, libelle_consommable AS type, num_appartement AS appartement
     FROM consomme
     INNER JOIN consommable ON consomme.id_consommable = consommable.id_consommable
-    ORDER BY date_conso;"""
+    ORDER BY date_conso DESC;'''
     mycursor.execute(sql)
     liste_consommations = mycursor.fetchall()
     return render_template("consommations/show_consommations.html", conso=liste_consommations)
@@ -73,20 +73,85 @@ def delete_consommations():
     id=request.args.get('id',0)
     return redirect('/consommations/show')
 
-@app.route('/etudiant/edit', methods=['GET'])
-def edit_etudiant():
-    print('''affichage du formulaire pour modifier un étudiant''')
+@app.route('/consommations/edit', methods=['GET'])
+def edit_consommation():
+    print('''affichage du formulaire pour modifier une consommation''')
     print(request.args)
     print(request.args.get('id'))
+    
     id=request.args.get('id')
     mycursor = get_db().cursor()
-    sql=''' SELECT id_etudiant AS id, nom_etudiant AS nom, groupe_etudiant AS groupe
-    FROM etudiant
-    WHERE id_etudiant=%s;'''
+    sql = '''SELECT id_consomme AS id, date_conso AS date, quantite_consomme AS quantite, id_consommable AS idc, num_appartement AS appartement
+    FROM consomme
+    WHERE id_consomme=%s;'''
     tuple_param=(id)
     mycursor.execute(sql,tuple_param)
-    etudiant = mycursor.fetchone()
-    return render_template('etudiant/edit_etudiant.html', etudiant=etudiant)
+    consommation = mycursor.fetchone()
+    
+    sql = '''SELECT id_consommable AS id, libelle_consommable AS type
+    FROM consommable'''
+    mycursor.execute(sql)
+    consommable = mycursor.fetchall()
+    
+    sql = '''SELECT num_appartement AS num_app
+    FROM appartement'''
+    mycursor.execute(sql)
+    appart = mycursor.fetchall()
+    return render_template('consommations/edit_consommation.html', consommation=consommation, consommable=consommable, appart=appart)
+
+@app.route('/consommations/edit', methods=['POST'])
+def valid_edit_consommation():
+    print('''modification de la consommation dans le tableau''')
+    id = request.form.get('id')
+    date = request.form.get('date')
+    quantite = request.form.get('quantite')
+    type = request.form.get('type')
+    appartement = request.form.get('appartement')
+    message = 'date:' + date + ' - quantite:' + quantite + ' - type:' + type + ' - appartement:' + appartement + ' - pour la consommation d identifiant:' + id
+    print(message)
+    mycursor = get_db().cursor()
+    tuple_param=(date,quantite,type,appartement,id)
+    sql="UPDATE consomme SET date_conso = %s, quantite_consomme= %s, id_consommable= %s, num_appartement= %s WHERE id_consomme=%s;"
+    mycursor.execute(sql,tuple_param)
+    get_db().commit()
+    return redirect('/consommations/show')
+
+@app.route('/consommations/add', methods=['GET'])
+def add_consommation():
+    print('''affichage du formulaire pour saisir un consommable''')
+
+    mycursor = get_db().cursor()
+    sql = '''SELECT id_consomme AS id, date_conso AS date, quantite_consomme AS quantite, id_consommable AS idc, num_appartement AS appartement
+    FROM consomme'''
+    mycursor.execute(sql)
+    consommation = mycursor.fetchall()
+    
+    sql = '''SELECT id_consommable AS id, libelle_consommable AS type
+    FROM consommable'''
+    mycursor.execute(sql)
+    consommable = mycursor.fetchall()
+    
+    sql = '''SELECT num_appartement AS num_app
+    FROM appartement'''
+    mycursor.execute(sql)
+    appart = mycursor.fetchall()
+    return render_template('consommations/add_consommation.html', consommation=consommation, consommable=consommable, appart=appart)
+
+@app.route('/consommations/add', methods=['POST'])
+def valid_add_consommation():
+    print('''ajout du consommable dans le tableau''')
+    date=request.form.get('date')
+    quantite=request.form.get('quantite')
+    type=request.form.get('type')
+    appartement=request.form.get('appartement')
+    message = 'date:' + date + ' - quantite:' + quantite + ' - type:' + type + ' - appartement:' + appartement
+    print(message)
+    mycursor = get_db().cursor()
+    tuple_param=(date,quantite, type, appartement)
+    sql="INSERT INTO consomme(id_consomme, date_conso, quantite_consomme, id_consommable, num_appartement) VALUES (NULL, %s, %s, %s, %s);"
+    mycursor.execute(sql,tuple_param)
+    get_db().commit()
+    return redirect('/consommations/show')
 
 # == Routes contrats == #
 
